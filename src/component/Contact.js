c8d12f8b2d463bdbe63136ca03f1a87565dadc7f
Divya <Divya123@gmail.com>
2023-11-20 13:29:53 +0530

    added

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,17 +1,34 @@
-import "../css/Contact.css";
-function Contact() {
+import "../css/Contact.css"
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+function Contact(){
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_o3a7lxb', 'template_hadgnjk', form.current, '0V8Qk1_vZyY_hLFzK')
+      .then((result) => {
+          alert("message sent");
+          form.current.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
-    <>
-      <div id="contact">
-        <h2>Contact</h2>
-        <p>
-          <ul>
-            <li>Mail:divyashree2004@gmail.com</li>
-            <li>Mobile No:86367688</li>
-          </ul>
-        </p>
-      </div>
-    </>
+    <div id="contact">
+      <h2>Contact</h2>
+      <div className="container1">
+        <h3>Send us a message</h3>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name:</label>
+      <input type="text" name="user_name" /><br/>
+      <label>Email:</label>
+      <input type="email" name="user_email" /><br/>
+      <label>Message:</label>
+      <textarea name="message" /><br/>
+      <input type="submit" value="Send" /><br/>
+    </form></div></div>
   );
 }
 export default Contact;
